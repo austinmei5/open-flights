f4ea3145dce45f6f1fb9c409abe3fffda763329b
austinmei5 <austinmei@uchicago.edu>
2023-03-10 18:22:36 +0000

    adding rating system to review

--- app/javascript/components/Airline/Review.js
+++ app/javascript/components/Airline/Review.js
@@ -1,16 +1,35 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid rgba(0,0,0,0.1);
+  border-radius: 4px;
+  padding: 20px;
+  margin: 0 40px 10px 0;
+`
+const RatingContainer = styled.div``
+const RatingScore = styled.div``
+const Title = styled.div`
+  padding: 10px 0 0 0;
+  font-size: 18px;
+
+`
+const Description = styled.div`
+  padding: 10px 0 10px 0;
+  font-size 14px;
+`
 
 const Review = (props) => {
   const {score, title, description} = props.attributes
 
   return (
-    <div className="card">
+    <Card>
       <div className="rating-container">
         <div className="rating-score">{score}</div>
       </div>
-      <div className="title">{title}</div>
-      <div className="description">{description}</div>
-    </div>
+      <Title>{title}</Title>
+      <Description>{description}</Description>
+    </Card>
   )
 }
 
