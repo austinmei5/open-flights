6d59b1468cc17be7040115ff7705d0b893056b7f
austinmei5 <austinmei@uchicago.edu>
2023-03-10 04:56:00 +0000

    create header

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,15 +1,34 @@
 import React,  { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useParams } from 'react-router-dom'
+import Header from './Header'
 
 const Airline = () => {
   const [airline, setAirline] = useState({})
   const [review, setReview] = useState({})
 
+  const { slug } = useParams()
+
   useEffect(() => {
-    
-  })
+    //airlines/united-airlines 
+    const url = `/api/v1/airlines/${slug}`
+
+    axios.get(url)
+    .then( response => setAirline(response.data))
+    .catch( response => console.log(response))
+  }, [])
 
 
-  return <div>This is the Airlines#show view for our app</div>
+  return (
+  <div className="wrapper">
+    <div className="column">
+      <div className="header"></div>
+      <div classname="reviews"></div>
+    </div>
+    <div className="column">
+      <div className="review-form">[Review Form Location</div>
+    </div>
+  </div>)
 }
 
 
