c02c703b30fe807dc3de27d2589027b65eaa1238
austinmei5 <austinmei@uchicago.edu>
2023-03-10 03:58:59 +0000

    start front page cards

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,15 +1,36 @@
 import React, { useState, useEffect } from 'react'
-
+import axios from 'axios'
+import Airline from './Airline'
+  
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
 
   useEffect(() => {
-    
-  }, [])
+    axios.get('/api/v1/airlines.json')
+    .then( response => {
+      setAirlines(response.data.data)
+    })
+    .catch( response => console.log(response))
+
+  }, [airlines.length])
+
+  const grid = airlines.map( item => {
+    return (
+      <Airline key={item.attributes.name} attributes={item.attributes} />
+    )
+  })
 
 
   return (
-    <div>This is the Airlines#index view for our app</div>
+    <div className="home">
+      <div className="header">
+        <h1>OpenFlights</h1>
+        <div className="subheader">Honest, unbiased airline reviews.</div>
+      </div>
+      <div className="grid">
+        <ul>{list}</ul>
+      </div>
+    </div>
   )
 }
 
